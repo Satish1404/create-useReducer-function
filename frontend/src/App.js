d6c010cb742124805cee29a58f35ca328036e50f
satish1404 <satishthakare2005@gmail.com>
2024-07-21 17:38:38 +0530

    create useReducer Function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useReducer } from "react";
 import './App.css';
 
+let oldState = {
+  value: 0,
+};
+
+const reducerFunction =(oldState, action)=>{
+  console.log("action--->",action.type);
+  console.log("oldState--->",oldState);
+  switch(action.type){
+    case"ADDITION":
+    return {...oldState, value:oldState.value+action.payload};
+      //break;
+    case"SUBTRACTION":
+    return {...oldState, value:oldState.value-action.payload};
+      //break;
+     default:
+      return oldState;   
+  }
+
+}
+
 function App() {
+
+  //1.hook area
+
+  const [newState,dispatch] = useReducer(reducerFunction,oldState);
+
+
+
+  //2.function defination area
+
+
+
+  //3.return statement
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{newState.value}</h1>
+      <button type="button" onClick={()=>{dispatch({type:"ADDITION",payload:5})}}>+</button>
+      <button type="button" onClick={()=>{dispatch({type:"SUBTRACTION",payload:5})}}>-</button>
     </div>
   );
 }
